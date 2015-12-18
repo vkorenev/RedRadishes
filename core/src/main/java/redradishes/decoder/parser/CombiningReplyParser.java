c334e6f0208b5918f679a6a3d6c0bfe78cabbef3
Vladimir Korenev <vkorenev@gmail.com>
2015-12-18 21:09:09 +0200

    Make CombiningReplyParser an interface

--- core/src/main/java/redradishes/decoder/parser/CombiningReplyParser.java
+++ core/src/main/java/redradishes/decoder/parser/CombiningReplyParser.java
@@ -3,46 +3,77 @@ package redradishes.decoder.parser;
 import javax.annotation.Nullable;
 import java.nio.ByteBuffer;
 import java.nio.charset.CharsetDecoder;
+import java.util.Objects;
 import java.util.function.BiFunction;
 import java.util.function.Function;
 
-class CombiningReplyParser<T1, T2, R> implements ReplyParser<R> {
-  private final ReplyParser<? extends T1> parser1;
-  private final ReplyParser<? extends T2> parser2;
-  private final BiFunction<? super T1, ? super T2, ? extends R> fn;
+interface CombiningReplyParser<T1, T2> {
+  static <T1, T2> CombiningReplyParser<T1, T2> combine(ReplyParser<? extends T1> parser1,
+      ReplyParser<? extends T2> parser2) {
+    return new CombiningReplyParser<T1, T2>() {
+      @Override
+      public <U> U parseReply(ByteBuffer buffer, BiFunction<? super T1, ? super T2, ? extends U> resultHandler,
+          PartialReplyHandler<? super T1, ? super T2, U> partialReplyHandler,
+          ReplyParser.FailureHandler<U> failureHandler, CharsetDecoder charsetDecoder) {
+        return parse1(parser1, buffer, resultHandler, partialReplyHandler, failureHandler, charsetDecoder);
+      }
 
-  CombiningReplyParser(ReplyParser<? extends T1> parser1, ReplyParser<? extends T2> parser2,
-      BiFunction<? super T1, ? super T2, ? extends R> fn) {
-    this.parser1 = parser1;
-    this.parser2 = parser2;
-    this.fn = fn;
-  }
-
-  @Override
-  public <U> U parseReply(ByteBuffer buffer, Function<? super R, U> resultHandler,
-      PartialReplyHandler<? super R, U> partialReplyHandler, FailureHandler<U> failureHandler,
-      CharsetDecoder charsetDecoder) {
-    return parser1.parseReply(buffer, new Function<T1, U>() {
+      private <U> U parse1(ReplyParser<? extends T1> parser1, ByteBuffer buffer,
+          BiFunction<? super T1, ? super T2, ? extends U> resultHandler,
+          PartialReplyHandler<? super T1, ? super T2, U> partialReplyHandler,
+          ReplyParser.FailureHandler<U> failureHandler, CharsetDecoder charsetDecoder) {
+        return parser1.parseReply(buffer, new Function<T1, U>() {
           @Override
           public U apply(@Nullable T1 value1) {
             return parse2(buffer, resultHandler, partialReplyHandler, failureHandler, value1, parser2);
           }
 
-          private <U1> U1 parse2(ByteBuffer buffer, Function<? super R, U1> resultHandler,
-              PartialReplyHandler<? super R, U1> partialReplyHandler, FailureHandler<U1> failureHandler,
-              @Nullable T1 value1, ReplyParser<? extends T2> parser2) {
-            return parser2.parseReply(buffer, value2 -> resultHandler.apply(fn.apply(value1, value2)),
-                partial -> partialReplyHandler.partialReply(new ReplyParser<R>() {
+          private <U1> U1 parse2(ByteBuffer buffer, BiFunction<? super T1, ? super T2, ? extends U1> resultHandler,
+              PartialReplyHandler<? super T1, ? super T2, U1> partialReplyHandler,
+              ReplyParser.FailureHandler<U1> failureHandler, @Nullable T1 value1, ReplyParser<? extends T2> parser2) {
+            return parser2.parseReply(buffer, value2 -> resultHandler.apply(value1, value2),
+                partial -> partialReplyHandler.partialReply(new CombiningReplyParser<T1, T2>() {
                   @Override
-                  public <U2> U2 parseReply(ByteBuffer buffer, Function<? super R, U2> resultHandler,
-                      PartialReplyHandler<? super R, U2> partialReplyHandler, FailureHandler<U2> failureHandler,
-                      CharsetDecoder charsetDecoder) {
+                  public <U2> U2 parseReply(ByteBuffer buffer,
+                      BiFunction<? super T1, ? super T2, ? extends U2> resultHandler,
+                      PartialReplyHandler<? super T1, ? super T2, U2> partialReplyHandler,
+                      ReplyParser.FailureHandler<U2> failureHandler, CharsetDecoder charsetDecoder) {
                     return parse2(buffer, resultHandler, partialReplyHandler, failureHandler, value1, partial);
                   }
                 }), failureHandler, charsetDecoder);
           }
-        }, partial -> partialReplyHandler.partialReply(new CombiningReplyParser<>(partial, parser2, fn)),
-        failureHandler,
-        charsetDecoder);
+        }, partial -> partialReplyHandler.partialReply(new CombiningReplyParser<T1, T2>() {
+          @Override
+          public <U1> U1 parseReply(ByteBuffer buffer, BiFunction<? super T1, ? super T2, ? extends U1> resultHandler,
+              PartialReplyHandler<? super T1, ? super T2, U1> partialReplyHandler,
+              ReplyParser.FailureHandler<U1> failureHandler, CharsetDecoder charsetDecoder) {
+            return parse1(partial, buffer, resultHandler, partialReplyHandler, failureHandler, charsetDecoder);
+          }
+        }), failureHandler, charsetDecoder);
+      }
+    };
+  }
+
+  <U> U parseReply(ByteBuffer buffer, BiFunction<? super T1, ? super T2, ? extends U> resultHandler,
+      PartialReplyHandler<? super T1, ? super T2, U> partialReplyHandler, ReplyParser.FailureHandler<U> failureHandler,
+      CharsetDecoder charsetDecoder);
+
+  default <R> ReplyParser<R> mapToParser(BiFunction<? super T1, ? super T2, ? extends R> mapper) {
+    Objects.requireNonNull(mapper);
+    return new ReplyParser<R>() {
+      @Override
+      public <U> U parseReply(ByteBuffer buffer, Function<? super R, U> resultHandler,
+          PartialReplyHandler<? super R, U> partialReplyHandler, FailureHandler<U> failureHandler,
+          CharsetDecoder charsetDecoder) {
+        return CombiningReplyParser.this
+            .parseReply(buffer, (value1, value2) -> resultHandler.apply(mapper.apply(value1, value2)),
+                partial -> partialReplyHandler.partialReply(partial.mapToParser(mapper)), failureHandler,
+                charsetDecoder);
+      }
+    };
+  }
+
+  interface PartialReplyHandler<T1, T2, R> {
+    R partialReply(CombiningReplyParser<? extends T1, ? extends T2> partial);
   }
 }
